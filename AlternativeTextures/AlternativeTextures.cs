88824de5cd7fa75fa8f52939d6710aac53722a91
Mr. Automation <31755155+Floogen@users.noreply.github.com>
2021-07-30 20:08:06 -0500

    Rough outline for AT example pack

--- AlternativeTextures/AlternativeTextures.cs
+++ AlternativeTextures/AlternativeTextures.cs
@@ -1,6 +1,8 @@
 ﻿using StardewModdingAPI;
+using StardewModdingAPI.Events;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +11,44 @@ namespace AlternativeTextures
 {
     public class AlternativeTextures : Mod
     {
+        internal static IMonitor monitor;
+        internal static IModHelper modHelper;
+
         public override void Entry(IModHelper helper)
         {
+            // Set up the monitor and helper
+            monitor = Monitor;
+            modHelper = Helper;
 
+            // Hook into GameLoop events
+            helper.Events.GameLoop.GameLaunched += this.OnGameLaunched;
+        }
+
+        private void OnGameLaunched(object sender, GameLaunchedEventArgs e)
+        {
+            // Hook into the APIs we utilize
+            // TODO: Hook into Json Asset API to match for any JA Content Pack by name
+
+            // Load any owned content packs
+            this.LoadContentPacks();
+        }
+
+        private void LoadContentPacks(bool isReload = false)
+        {
+            // Load owned content packs
+            foreach (IContentPack contentPack in Helper.ContentPacks.GetOwned())
+            {
+                Monitor.Log($"Loading companions from pack: {contentPack.Manifest.Name} {contentPack.Manifest.Version} by {contentPack.Manifest.Author}", LogLevel.Debug);
+
+                var textureFolders = new DirectoryInfo(Path.Combine(contentPack.DirectoryPath, "Textures")).GetDirectories();
+                if (textureFolders.Count() == 0)
+                {
+                    Monitor.Log($"No sub-folders found under Textures for the content pack {contentPack.Manifest.Name}!", LogLevel.Warn);
+                    continue;
+                }
+
+
+            }
         }
     }
 }
