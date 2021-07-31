6e5bf8484e56d441d97f7ac29ac1d58efbb17198
Mr. Automation <31755155+Floogen@users.noreply.github.com>
2021-07-30 19:28:21 -0500

    Start of project

---
+++ AlternativeTextures/AlternativeTextures.cs
@@ -0,0 +1,17 @@
+﻿using StardewModdingAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlternativeTextures
+{
+    public class AlternativeTextures : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+
+        }
+    }
+}
