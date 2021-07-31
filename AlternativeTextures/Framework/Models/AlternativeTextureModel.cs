780dd88f1131fed052d762fae6f794ce6cd1b3ed
Mr. Automation <31755155+Floogen@users.noreply.github.com>
2021-07-30 21:50:40 -0500

    Implemented AlternativeTextureModel

---
+++ AlternativeTextures/Framework/Models/AlternativeTextureModel.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using StardewModdingAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlternativeTextures.Framework.Models
+{
+    public class AlternativeTextureModel
+    {
+        public string Owner { get; set; }
+        public string ItemName { get; set; }
+        public int ItemId { get; set; } = -1;
+        public ITranslationHelper Translations { get; set; }
+        public string Type { get; set; }
+        public int TextureWidth { get; set; }
+        public int TextureHeight { get; set; }
+        public int Variations { get; set; } = 1;
+        public Texture2D Texture { get; set; }
+        public string TileSheetPath { get; set; }
+
+        public string GetId()
+        {
+            return String.Concat(Owner, ":", ItemId);
+        }
+
+        public override string ToString()
+        {
+            return $"\n[\n" +
+                $"\tOwner: {Owner} | ItemName: {ItemName} | ItemId: {ItemId} | Type: {Type}\n" +
+                $"\tTextureWidth x TextureHeight: [{TextureWidth}x{TextureHeight}] | Variations: {Variations}\n";
+        }
+    }
+}
