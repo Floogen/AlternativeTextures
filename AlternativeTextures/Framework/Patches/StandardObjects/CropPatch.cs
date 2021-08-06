aa3828c806632e0c0ad6cdba106cc8803b9aceaa
Mr. Automation <31755155+Floogen@users.noreply.github.com>
2021-08-06 18:05:47 -0500

    Added color tint handling and forage for crop draw patches

--- AlternativeTextures/Framework/Patches/StandardObjects/CropPatch.cs
+++ AlternativeTextures/Framework/Patches/StandardObjects/CropPatch.cs
@@ -31,7 +31,7 @@ namespace AlternativeTextures.Framework.Patches.StandardObjects
             harmony.Patch(AccessTools.Method(_object, nameof(Crop.drawWithOffset), new[] { typeof(SpriteBatch), typeof(Vector2), typeof(Color), typeof(float), typeof(Vector2) }), prefix: new HarmonyMethod(GetType(), nameof(DrawWithOffsetPrefix)));
         }
 
-        private static bool DrawPrefix(Crop __instance, Vector2 ___origin, Vector2 ___drawPosition, SpriteBatch b, Vector2 tileLocation, Color toTint, float rotation)
+        private static bool DrawPrefix(Crop __instance, Vector2 ___origin, Vector2 ___drawPosition, Rectangle ___coloredSourceRect, float ___coloredLayerDepth, Vector2 ___smallestTileSizeOrigin, float ___layerDepth, SpriteBatch b, Vector2 tileLocation, Color toTint, float rotation)
         {
             var hoeDirt = Game1.currentLocation.terrainFeatures[tileLocation] as HoeDirt;
             if (hoeDirt != null && hoeDirt.modData.ContainsKey("AlternativeTextureName"))
@@ -47,14 +47,38 @@ namespace AlternativeTextures.Framework.Patches.StandardObjects
                 {
                     return true;
                 }
+                var textureOffset = textureVariation * textureModel.TextureHeight;
 
                 Vector2 position = Game1.GlobalToLocal(Game1.viewport, ___drawPosition);
+
+                // Handle drawing forages
+                if ((bool)__instance.forageCrop)
+                {
+                    if ((int)__instance.whichForageCrop == 2)
+                    {
+                        b.Draw(textureModel.Texture, Game1.GlobalToLocal(Game1.viewport, new Vector2(tileLocation.X * 64f + ((tileLocation.X * 11f + tileLocation.Y * 7f) % 10f - 5f) + 32f, tileLocation.Y * 64f + ((tileLocation.Y * 11f + tileLocation.X * 7f) % 10f - 5f) + 64f)), new Rectangle(0, textureOffset, 16, 16), Color.White, rotation, new Vector2(8f, 16f), 4f, SpriteEffects.None, (tileLocation.Y * 64f + 32f + ((tileLocation.Y * 11f + tileLocation.X * 7f) % 10f - 5f)) / 10000f);
+                    }
+                    else
+                    {
+                        b.Draw(textureModel.Texture, position, new Rectangle(0, textureOffset, 16, 16), Color.White, 0f, ___smallestTileSizeOrigin, 4f, SpriteEffects.None, ___layerDepth);
+                    }
+
+                    return false;
+                }
+
+                // Handle the crops / flowers
                 SpriteEffects effect = (__instance.flip ? SpriteEffects.FlipHorizontally : SpriteEffects.None);
                 var layerDepth = (tileLocation.Y * 64f + 32f + ((!__instance.shouldDrawDarkWhenWatered() || (int)__instance.currentPhase >= __instance.phaseDays.Count - 1) ? 0f : ((tileLocation.Y * 11f + tileLocation.X * 7f) % 10f - 5f))) / 10000f / (((int)__instance.currentPhase == 0 && __instance.shouldDrawDarkWhenWatered()) ? 2f : 1f);
                 var sourceX = ((!__instance.fullyGrown) ? ((int)(((int)__instance.phaseToShow != -1) ? __instance.phaseToShow : __instance.currentPhase) + (((int)(((int)__instance.phaseToShow != -1) ? __instance.phaseToShow : __instance.currentPhase) == 0 && ((int)tileLocation.X * 7 + (int)tileLocation.Y * 11) % 2 == 0) ? (-1) : 0) + 1) : (((int)__instance.dayOfCurrentPhase <= 0) ? 6 : 7)) * 16;
 
-                // TODO: Handle color overlays
-                b.Draw(textureModel.Texture, position, new Rectangle(sourceX, (textureVariation * textureModel.TextureHeight), 16, 32), toTint, rotation, ___origin, 4f, effect, layerDepth);
+                b.Draw(textureModel.Texture, position, new Rectangle(sourceX, textureOffset, 16, 32), toTint, rotation, ___origin, 4f, effect, layerDepth);
+
+                // Handle the tinted colors for flowers
+                Color tintColor = __instance.tintColor.Value;
+                if (!tintColor.Equals(Color.White) && (int)__instance.currentPhase == __instance.phaseDays.Count - 1 && !__instance.dead)
+                {
+                    b.Draw(textureModel.Texture, position, new Rectangle(___coloredSourceRect.X, textureOffset, ___coloredSourceRect.Width, ___coloredSourceRect.Height), tintColor, rotation, ___origin, 4f, effect, ___coloredLayerDepth);
+                }
 
                 return false;
             }
