a61fbdbfbf968e8ff0f532daeb0e669269509db0
Mr. Automation <31755155+Floogen@users.noreply.github.com>
2021-08-11 23:00:28 -0500

    Implemented DrawInMenuPrefix

--- AlternativeTextures/Framework/Patches/StandardObjects/FlooringPatch.cs
+++ AlternativeTextures/Framework/Patches/StandardObjects/FlooringPatch.cs
@@ -29,6 +29,7 @@ namespace AlternativeTextures.Framework.Patches.StandardObjects
         internal void Apply(Harmony harmony)
         {
             harmony.Patch(AccessTools.Method(_object, nameof(Flooring.draw), new[] { typeof(SpriteBatch), typeof(Vector2) }), prefix: new HarmonyMethod(GetType(), nameof(DrawPrefix)));
+            harmony.Patch(AccessTools.Method(_object, nameof(Flooring.drawInMenu), new[] { typeof(SpriteBatch), typeof(Vector2), typeof(Vector2), typeof(float), typeof(float) }), prefix: new HarmonyMethod(GetType(), nameof(DrawInMenuPrefix)));
             harmony.Patch(AccessTools.Method(_object, nameof(Flooring.seasonUpdate), new[] { typeof(bool) }), postfix: new HarmonyMethod(GetType(), nameof(SeasonUpdatePostfix)));
         }
 
@@ -108,6 +109,57 @@ namespace AlternativeTextures.Framework.Patches.StandardObjects
                 }
 
                 spriteBatch.Draw(textureModel.Texture, Game1.GlobalToLocal(Game1.viewport, new Vector2(tileLocation.X * 64f, tileLocation.Y * 64f)), new Rectangle(sourceRectPosition * 16 % 256, (sourceRectPosition / 16 * 16) + textureOffset, 16, 16), Color.White, 0f, Vector2.Zero, 4f, SpriteEffects.None, 1E-09f);
+
+                return false;
+            }
+            return true;
+        }
+
+        private static bool DrawInMenuPrefix(Flooring __instance, SpriteBatch spriteBatch, Vector2 positionOnScreen, Vector2 tileLocation, float scale, float layerDepth)
+        {
+
+            if (__instance.modData.ContainsKey("AlternativeTextureName"))
+            {
+                var textureModel = AlternativeTextures.textureManager.GetSpecificTextureModel(__instance.modData["AlternativeTextureName"]);
+                if (textureModel is null)
+                {
+                    return true;
+                }
+
+                var textureVariation = Int32.Parse(__instance.modData["AlternativeTextureVariation"]);
+                if (textureVariation == -1)
+                {
+                    return true;
+                }
+                var textureOffset = textureVariation * textureModel.TextureHeight;
+
+                int sourceRectPosition = 1;
+                byte drawSum = 0;
+                Vector2 surroundingLocations = tileLocation;
+                surroundingLocations.X += 1f;
+                GameLocation farm = Game1.getLocationFromName("Farm");
+                if (farm.terrainFeatures.ContainsKey(surroundingLocations) && farm.terrainFeatures[surroundingLocations] is Flooring)
+                {
+                    drawSum = (byte)(drawSum + 2);
+                }
+                surroundingLocations.X -= 2f;
+                if (farm.terrainFeatures.ContainsKey(surroundingLocations) && Game1.currentLocation.terrainFeatures[surroundingLocations] is Flooring)
+                {
+                    drawSum = (byte)(drawSum + 8);
+                }
+                surroundingLocations.X += 1f;
+                surroundingLocations.Y += 1f;
+                if (Game1.currentLocation.terrainFeatures.ContainsKey(surroundingLocations) && farm.terrainFeatures[surroundingLocations] is Flooring)
+                {
+                    drawSum = (byte)(drawSum + 4);
+                }
+                surroundingLocations.Y -= 2f;
+                if (farm.terrainFeatures.ContainsKey(surroundingLocations) && farm.terrainFeatures[surroundingLocations] is Flooring)
+                {
+                    drawSum = (byte)(drawSum + 1);
+                }
+                sourceRectPosition = Flooring.drawGuide[drawSum];
+                spriteBatch.Draw(textureModel.Texture, positionOnScreen, new Rectangle(sourceRectPosition % 16 * 16, sourceRectPosition / 16 * 16 + textureOffset, 16, 16), Color.White, 0f, Vector2.Zero, scale * 4f, SpriteEffects.None, layerDepth + positionOnScreen.Y / 20000f);
 
                 return false;
             }
