b0c50f3fb91fac88e7fe7d3e959a8169513c9446
Mr. Automation <31755155+Floogen@users.noreply.github.com>
2021-08-13 12:41:16 -0500

    Rewrote Chest.draw prefix to mirror vanilla logic better

--- AlternativeTextures/Framework/Patches/AnimatedObjects/ChestPatch.cs
+++ AlternativeTextures/Framework/Patches/AnimatedObjects/ChestPatch.cs
@@ -12,6 +12,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static StardewValley.Objects.Chest;
 using Object = StardewValley.Object;
 
 namespace AlternativeTextures.Framework.Patches.AnimatedObjects
@@ -31,7 +32,7 @@ namespace AlternativeTextures.Framework.Patches.AnimatedObjects
             harmony.Patch(AccessTools.Method(_object, nameof(Chest.draw), new[] { typeof(SpriteBatch), typeof(int), typeof(int), typeof(float), typeof(bool) }), prefix: new HarmonyMethod(GetType(), nameof(DrawRecolorPrefix)));
         }
 
-        private static bool DrawPrefix(Chest __instance, int ____shippingBinFrameCounter, SpriteBatch spriteBatch, int x, int y, float alpha = 1f)
+        private static bool DrawPrefix(Chest __instance, int ___currentLidFrame, int ____shippingBinFrameCounter, SpriteBatch spriteBatch, int x, int y, float alpha = 1f)
         {
             if (__instance.modData.ContainsKey("AlternativeTextureName"))
             {
@@ -55,17 +56,28 @@ namespace AlternativeTextures.Framework.Patches.AnimatedObjects
                     draw_x = Utility.Lerp(__instance.localKickStartTile.Value.X, draw_x, __instance.kickProgress);
                     draw_y = Utility.Lerp(__instance.localKickStartTile.Value.Y, draw_y, __instance.kickProgress);
                 }
-
                 float base_sort_order = Math.Max(0f, ((draw_y + 1f) * 64f - 24f) / 10000f) + draw_x * 1E-05f;
                 if (__instance.localKickStartTile.HasValue)
                 {
                     spriteBatch.Draw(Game1.shadowTexture, Game1.GlobalToLocal(Game1.viewport, new Vector2((draw_x + 0.5f) * 64f, (draw_y + 0.5f) * 64f)), Game1.shadowTexture.Bounds, Color.Black * 0.5f, 0f, new Vector2(Game1.shadowTexture.Bounds.Center.X, Game1.shadowTexture.Bounds.Center.Y), 4f, SpriteEffects.None, 0.0001f);
                     draw_y -= (float)Math.Sin((double)__instance.kickProgress * Math.PI) * 0.5f;
                 }
-
-                spriteBatch.Draw(textureModel.Texture, Game1.GlobalToLocal(Game1.viewport, new Vector2(draw_x * 64f + (float)((__instance.shakeTimer > 0) ? Game1.random.Next(-1, 2) : 0), (draw_y - 1f) * 64f)), new Rectangle(0, textureOffset, 16, 32), __instance.tint.Value * alpha, 0f, Vector2.Zero, 4f, SpriteEffects.None, base_sort_order);
-                spriteBatch.Draw(textureModel.Texture, Game1.GlobalToLocal(Game1.viewport, new Vector2(draw_x * 64f + (float)((__instance.shakeTimer > 0) ? Game1.random.Next(-1, 2) : 0), (draw_y - 1f) * 64f)), new Rectangle((1 + ____shippingBinFrameCounter) * textureModel.TextureWidth, textureOffset, 16, 32), __instance.tint.Value * alpha * alpha, 0f, Vector2.Zero, 4f, SpriteEffects.None, base_sort_order + 1E-05f);
-                return false;
+                if ((bool)__instance.playerChest && (__instance.ParentSheetIndex == 130 || __instance.ParentSheetIndex == 232))
+                {
+                    spriteBatch.Draw(textureModel.Texture, Game1.GlobalToLocal(Game1.viewport, new Vector2(draw_x * 64f + (float)((__instance.shakeTimer > 0) ? Game1.random.Next(-1, 2) : 0), (draw_y - 1f) * 64f)), new Rectangle(0, textureOffset, 16, 32), __instance.tint.Value * alpha, 0f, Vector2.Zero, 4f, SpriteEffects.None, base_sort_order);
+                    spriteBatch.Draw(textureModel.Texture, Game1.GlobalToLocal(Game1.viewport, new Vector2(draw_x * 64f + (float)((__instance.shakeTimer > 0) ? Game1.random.Next(-1, 2) : 0), (draw_y - 1f) * 64f)), new Rectangle((___currentLidFrame - __instance.parentSheetIndex) * textureModel.TextureWidth, textureOffset, 16, 32), __instance.tint.Value * alpha * alpha, 0f, Vector2.Zero, 4f, SpriteEffects.None, base_sort_order + 1E-05f);
+                    return false;
+                }
+                if ((bool)__instance.playerChest)
+                {
+                    spriteBatch.Draw(textureModel.Texture, Game1.GlobalToLocal(Game1.viewport, new Vector2(draw_x * 64f + (float)((__instance.shakeTimer > 0) ? Game1.random.Next(-1, 2) : 0), (draw_y - 1f) * 64f)), new Rectangle(0, textureOffset, 16, 32), __instance.tint.Value * alpha, 0f, Vector2.Zero, 4f, SpriteEffects.None, base_sort_order);
+                    spriteBatch.Draw(textureModel.Texture, Game1.GlobalToLocal(Game1.viewport, new Vector2(draw_x * 64f + (float)((__instance.shakeTimer > 0) ? Game1.random.Next(-1, 2) : 0), (draw_y - 1f) * 64f)), new Rectangle((___currentLidFrame - __instance.parentSheetIndex) * textureModel.TextureWidth, textureOffset, 16, 32), __instance.tint.Value * alpha * alpha, 0f, Vector2.Zero, 4f, SpriteEffects.None, base_sort_order + 1E-05f);
+                    return false;
+                }
+                if ((bool)__instance.giftbox)
+                {
+                    return true;
+                }
             }
             return true;
         }
