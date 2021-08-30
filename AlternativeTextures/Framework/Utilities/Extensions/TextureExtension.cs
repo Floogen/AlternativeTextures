b67826f00d65ec213cb2f5df885b29d2e9a8645e
Mr. Automation <31755155+Floogen@users.noreply.github.com>
2021-08-29 19:44:14 -0500

    Added try catch for CreateSelectiveCopy

--- AlternativeTextures/Framework/Utilities/Extensions/TextureExtension.cs
+++ AlternativeTextures/Framework/Utilities/Extensions/TextureExtension.cs
@@ -13,11 +13,19 @@ namespace AlternativeTextures.Framework.Utilities.Extensions
         public static Texture2D CreateSelectiveCopy(this Texture2D sourceTexture, GraphicsDevice device, Rectangle selectionRect)
         {
             Texture2D selectiveTexture = new Texture2D(device, selectionRect.Width, selectionRect.Height);
-            int dimensions = selectionRect.Width * selectionRect.Height;
-            Color[] data = new Color[dimensions];
+            try
+            {
 
-            sourceTexture.GetData(0, selectionRect, data, 0, dimensions);
-            selectiveTexture.SetData(data);
+                int dimensions = selectionRect.Width * selectionRect.Height;
+                Color[] data = new Color[dimensions];
+
+                sourceTexture.GetData(0, selectionRect, data, 0, dimensions);
+                selectiveTexture.SetData(data);
+            }
+            catch
+            {
+                AlternativeTextures.monitor.Log("Issue creating a selective texture copy, likely out of bounds for dimensions.", StardewModdingAPI.LogLevel.Trace);
+            }
 
             return selectiveTexture;
         }
