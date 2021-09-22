648a29b4f1c133aa5f5d8f4aa44e1a7c18ce6c8a
Mr. Automation <31755155+Floogen@users.noreply.github.com>
2021-09-21 20:54:40 -0500

    Implemented Tints property for crops.

--- AlternativeTextures/Framework/Patches/StandardObjects/CropPatch.cs
+++ AlternativeTextures/Framework/Patches/StandardObjects/CropPatch.cs
@@ -51,7 +51,7 @@ namespace AlternativeTextures.Framework.Patches.StandardObjects
 
 
         [HarmonyBefore(new string[] { "spacechase0.DynamicGameAssets" })]
-        private static bool DrawPrefix(Crop __instance, Vector2 ___origin, Vector2 ___drawPosition, Rectangle ___coloredSourceRect, float ___coloredLayerDepth, Vector2 ___smallestTileSizeOrigin, float ___layerDepth, SpriteBatch b, Vector2 tileLocation, Color toTint, float rotation)
+        private static bool DrawPrefix(Crop __instance, Vector2 ___origin, Vector2 ___drawPosition, Rectangle ___sourceRect, Rectangle ___coloredSourceRect, float ___coloredLayerDepth, Vector2 ___smallestTileSizeOrigin, float ___layerDepth, SpriteBatch b, Vector2 tileLocation, Color toTint, float rotation)
         {
             var hoeDirt = Game1.currentLocation.terrainFeatures[tileLocation] as HoeDirt;
             if (hoeDirt != null && hoeDirt.modData.ContainsKey("AlternativeTextureName"))
@@ -89,15 +89,20 @@ namespace AlternativeTextures.Framework.Patches.StandardObjects
                 // Handle the crops / flowers
                 SpriteEffects effect = (__instance.flip ? SpriteEffects.FlipHorizontally : SpriteEffects.None);
                 var layerDepth = (tileLocation.Y * 64f + 32f + ((!__instance.shouldDrawDarkWhenWatered() || (int)__instance.currentPhase >= __instance.phaseDays.Count - 1) ? 0f : ((tileLocation.Y * 11f + tileLocation.X * 7f) % 10f - 5f))) / 10000f / (((int)__instance.currentPhase == 0 && __instance.shouldDrawDarkWhenWatered()) ? 2f : 1f);
-                var sourceX = ((!__instance.fullyGrown) ? ((int)(((int)__instance.phaseToShow != -1) ? __instance.phaseToShow : __instance.currentPhase) + (((int)(((int)__instance.phaseToShow != -1) ? __instance.phaseToShow : __instance.currentPhase) == 0 && ((int)tileLocation.X * 7 + (int)tileLocation.Y * 11) % 2 == 0) ? (-1) : 0) + 1) : (((int)__instance.dayOfCurrentPhase <= 0) ? 6 : 7)) * 16;
+                var sourceX = ___sourceRect.X >= 128 ? ___sourceRect.X - 128 : ___sourceRect.X;
 
                 b.Draw(textureModel.GetTexture(textureVariation), position, new Rectangle(sourceX, textureOffset, 16, 32), toTint, rotation, ___origin, 4f, effect, layerDepth);
 
                 // Handle the tinted colors for flowers
                 Color tintColor = __instance.tintColor.Value;
-                if (!tintColor.Equals(Color.White) && (int)__instance.currentPhase == __instance.phaseDays.Count - 1 && !__instance.dead)
+                if ((!tintColor.Equals(Color.White) || textureModel.HasTint(textureVariation)) && (int)__instance.currentPhase == __instance.phaseDays.Count - 1 && !__instance.dead)
                 {
-                    b.Draw(textureModel.GetTexture(textureVariation), position, new Rectangle(___coloredSourceRect.X, textureOffset, ___coloredSourceRect.Width, ___coloredSourceRect.Height), tintColor, rotation, ___origin, 4f, effect, layerDepth + 0.01f);
+                    if (textureModel.HasTint(textureVariation))
+                    {
+                        tintColor = textureModel.GetRandomTint(textureVariation);
+                    }
+
+                    b.Draw(textureModel.GetTexture(textureVariation), position, new Rectangle(___coloredSourceRect.X >= 128 ? ___coloredSourceRect.X - 128 : ___coloredSourceRect.X, textureOffset, ___coloredSourceRect.Width, ___coloredSourceRect.Height), tintColor, rotation, ___origin, 4f, effect, layerDepth + 0.01f);
                 }
 
                 return false;
@@ -107,7 +112,7 @@ namespace AlternativeTextures.Framework.Patches.StandardObjects
 
 
         [HarmonyBefore(new string[] { "spacechase0.DynamicGameAssets" })]
-        private static bool DrawWithOffsetPrefix(Crop __instance, Vector2 ___origin, Vector2 ___drawPosition, Rectangle ___coloredSourceRect, SpriteBatch b, Vector2 tileLocation, Color toTint, float rotation, Vector2 offset)
+        private static bool DrawWithOffsetPrefix(Crop __instance, Vector2 ___origin, Vector2 ___drawPosition, Rectangle ___sourceRect, Rectangle ___coloredSourceRect, SpriteBatch b, Vector2 tileLocation, Color toTint, float rotation, Vector2 offset)
         {
             var gardenPot = Game1.currentLocation.getObjectAtTile((int)tileLocation.X, (int)tileLocation.Y) as IndoorPot;
             if (gardenPot is null)
@@ -131,17 +136,24 @@ namespace AlternativeTextures.Framework.Patches.StandardObjects
                 }
                 var textureOffset = textureVariation * textureModel.TextureHeight;
 
-                var sourceX = ((!__instance.fullyGrown) ? ((int)(((int)__instance.phaseToShow != -1) ? __instance.phaseToShow : __instance.currentPhase) + (((int)(((int)__instance.phaseToShow != -1) ? __instance.phaseToShow : __instance.currentPhase) == 0 && ((int)tileLocation.X * 7 + (int)tileLocation.Y * 11) % 2 == 0) ? (-1) : 0) + 1) : (((int)__instance.dayOfCurrentPhase <= 0) ? 6 : 7)) * 16;
+                var sourceX = ___sourceRect.X >= 128 ? ___sourceRect.X - 128 : ___sourceRect.X;
                 if ((bool)__instance.forageCrop)
                 {
                     b.Draw(textureModel.GetTexture(textureVariation), Game1.GlobalToLocal(Game1.viewport, offset + new Vector2(tileLocation.X * 64f, tileLocation.Y * 64f)), new Rectangle(sourceX, textureOffset, 16, 32), Color.White, 0f, new Vector2(8f, 8f), 4f, SpriteEffects.None, (tileLocation.Y + 0.66f) * 64f / 10000f + tileLocation.X * 1E-05f);
                     return false;
                 }
 
+
+                Color tintColor = __instance.tintColor.Value;
                 b.Draw(textureModel.GetTexture(textureVariation), Game1.GlobalToLocal(Game1.viewport, offset + new Vector2(tileLocation.X * 64f, tileLocation.Y * 64f)), new Rectangle(sourceX, textureOffset, 16, 32), toTint, rotation, new Vector2(8f, 24f), 4f, __instance.flip ? SpriteEffects.FlipHorizontally : SpriteEffects.None, (tileLocation.Y + 0.66f) * 64f / 10000f + tileLocation.X * 1E-05f);
-                if (!__instance.tintColor.Equals(Color.White) && (int)__instance.currentPhase == __instance.phaseDays.Count - 1 && !__instance.dead)
+                if ((!tintColor.Equals(Color.White) || textureModel.HasTint(textureVariation)) && (int)__instance.currentPhase == __instance.phaseDays.Count - 1 && !__instance.dead)
                 {
-                    b.Draw(textureModel.GetTexture(textureVariation), Game1.GlobalToLocal(Game1.viewport, offset + new Vector2(tileLocation.X * 64f, tileLocation.Y * 64f)), new Rectangle(___coloredSourceRect.X, textureOffset, ___coloredSourceRect.Width, ___coloredSourceRect.Height), __instance.tintColor, rotation, new Vector2(8f, 24f), 4f, __instance.flip ? SpriteEffects.FlipHorizontally : SpriteEffects.None, (tileLocation.Y + 0.67f) * 64f / 10000f + tileLocation.X * 1E-05f);
+                    if (textureModel.HasTint(textureVariation))
+                    {
+                        tintColor = textureModel.GetRandomTint(textureVariation);
+                    }
+
+                    b.Draw(textureModel.GetTexture(textureVariation), Game1.GlobalToLocal(Game1.viewport, offset + new Vector2(tileLocation.X * 64f, tileLocation.Y * 64f)), new Rectangle(___coloredSourceRect.X >= 128 ? ___coloredSourceRect.X - 128 : ___coloredSourceRect.X, textureOffset, ___coloredSourceRect.Width, ___coloredSourceRect.Height), tintColor, rotation, new Vector2(8f, 24f), 4f, __instance.flip ? SpriteEffects.FlipHorizontally : SpriteEffects.None, (tileLocation.Y + 0.67f) * 64f / 10000f + tileLocation.X * 1E-05f);
                 }
 
                 return false;
