861c1ce908cd4afaf91712d282030ae5481b1a91
Mr. Automation <31755155+Floogen@users.noreply.github.com>
2021-10-25 18:27:22 -0500

    Fixed spacing issue for Furniture items with height of 32, such as banners

--- AlternativeTextures/Framework/UI/PaintBucketMenu.cs
+++ AlternativeTextures/Framework/UI/PaintBucketMenu.cs
@@ -172,6 +172,10 @@ namespace AlternativeTextures.Framework.UI
                     {
                         _maxRows = 2;
                     }
+                    else if (sourceRect.Height >= 32)
+                    {
+                        _maxRows = 3;
+                    }
                     else if (sourceRect.Height <= 16)
                     {
                         sourceRect.Height = 32;
